83b0827e4d18b0b8e07a0f087cb3ae9a101af2b0
k-rite <44746687+k-rite@users.noreply.github.com>
2018-11-08 18:59:59 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -491,9 +491,64 @@ async def pepe(ctx, user: discord.Member = None):
 
 
     await ctx.send(embed=embed)
+@bot.command(pass_context=True, name='youtube', no_pm=True)
+async def youtube(ctx, *, query: str):
+    """Search on Youtube"""
+    try:
+        url = 'https://www.youtube.com/results?'
+        payload = {'search_query': ''.join(query)}
+        headers = {'user-agent': 'Red-cog/1.0'}
+        conn = aiohttp.TCPConnector()
+        session = aiohttp.ClientSession(connector=conn)
+        async with session.get(url, params=payload, headers=headers) as r:
+            result = await r.text()
+        session.close()
+        yt_find = re.findall(r'href=\"\/watch\?v=(.{11})', result)
+        url = 'https://www.youtube.com/watch?v={}'.format(yt_find[0])
+        await ctx.send (url)
+    except Exception as e:
+        message = 'Something went terribly wrong! [{}]'.format(e)
+        await  ctx.send(message)
+
+@bot.command(pass_context=True, name='wikipedia', aliases=['wiki', 'w'])
+async def wikipedia(ctx, *, query: str):
+    """
+    Get information from Wikipedia
+    """
+    try:
+        url = 'https://en.wikipedia.org/w/api.php?'
+        payload = {}
+        payload['action'] = 'query'
+        payload['format'] = 'json'
+        payload['prop'] = 'extracts'
+        payload['titles'] = ''.join(query).replace(' ', '_')
+        payload['exsentences'] = '5'
+        payload['redirects'] = '1'
+        payload['explaintext'] = '1'
+        headers = {'user-agent': 'Red-cog/1.0'}
+        conn = aiohttp.TCPConnector(verify_ssl=False)
+        session = aiohttp.ClientSession(connector=conn)
+        async with session.get(url, params=payload, headers=headers) as r:
+            result = await r.json()
+        session.close()
+        if '-1' not in result['query']['pages']:
+            for page in result['query']['pages']:
+                title = result['query']['pages'][page]['title']
+                description = result['query']['pages'][page]['extract'].replace('\n', '\n\n')
+            em = discord.Embed(title='Wikipedia: {}'.format(title), description=u'\u2063\n{}...\n\u2063'.format(description[:-3]), color=discord.Color.blue(), url='https://en.wikipedia.org/wiki/{}'.format(title.replace(' ', '_')))
+            em.set_footer(text='Information provided by Wikimedia', icon_url='https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Wikimedia-logo.png/600px-Wikimedia-logo.png')
+            await ctx.send(embed=em)
+        else:
+            message = 'I\'m sorry, I can\'t find {}'.format(''.join(query))
+            await ctx.send('```{}```'.format(message))
+    except Exception as e:
+        message = 'Something went terribly wrong! [{}]'.format(e)
+        await ctx.send('```{}```'.format(message))
 
     
-
+@bot.listen()
+async def on_member_join(member):
+    await member.send(f"{member.name}Welcome to {member.guild.name}")
 
 
 @bot.event
@@ -518,8 +573,6 @@ async def on_message(msg):
 
 @bot.event
 async def on_ready():
-    bot.load_extension("fun")
-    bot.load_extension("search")
     bot.load_extension('ExampleRepl')
     options = ('help via s!help', 'to K-RITE` 🎧᳄#8172', f'on {len(bot.guilds)} servers')
     while True:
